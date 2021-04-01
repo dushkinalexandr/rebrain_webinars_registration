32008491a23f05683c7292f6f3f58cf732887c05
KU_22 <ku.wurm@google.com>
2021-04-01 15:31:23 +0300

    add send userdata to register form

--- registration.py
+++ registration.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 current_month = str.lower(datetime.datetime.now().strftime("%B"))
 current_year = str.lower(datetime.datetime.now().strftime("%Y"))
 link = f"https://rebrainme.com/webinars/devops-{current_month}-{current_year}/"
 
-user_name = "Xander"
-user_email = "123@gmail.com"
+user_name = "username"
+user_email = "example@example.edu"
 user_phone_number = "+79991235678"
 
 print(link)
@@ -22,5 +23,16 @@ button_register = browser.find_element(*BUTTON_REGISTER)
 browser.execute_script("return arguments[0].scrollIntoView(true);", button_register)
 button_register.click()
 
-time.sleep(3)
+
+USER_NAME_FIELD = (By.CSS_SELECTOR, ".userName")
+USER_EMAIL_FIELD = (By.CSS_SELECTOR, ".userEmail")
+USER_PHONE_FIELD = (By.CSS_SELECTOR, ".userPhone")
+BUTTON_REGISTER_FORM = (By.CSS_SELECTOR, ".inputform .btn-register")
+
+browser.find_element(*USER_NAME_FIELD).send_keys(user_name)
+browser.find_element(*USER_EMAIL_FIELD).send_keys(user_email)
+browser.find_element(*USER_PHONE_FIELD).send_keys(user_phone_number)
+# browser.find_element(*BUTTON_REGISTER_FORM).click()
+
+time.sleep(5)
 browser.close()
