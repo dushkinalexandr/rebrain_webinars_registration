45ab9eb7e145161732dc17530c8e0096e816fed9
KU_22 <ku.wurm@google.com>
2021-04-01 15:49:59 +0300

    add descriptions

--- registration.py
+++ registration.py
@@ -5,26 +5,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-#
+# Get current month and year
 current_month = str.lower(datetime.datetime.now().strftime("%B"))
 current_year = str.lower(datetime.datetime.now().strftime("%Y"))
 link = f"https://rebrainme.com/webinars/devops-{current_month}-{current_year}/"
+print(link)
 
+# User data
 user_name = "username"
 user_email = "example@example.edu"
 user_phone_number = "+79991235678"
 
-print(link)
-
+# Open browser
 browser = webdriver.Chrome()
 browser.get(link)
 
+# Find button registration to all webinars
 BUTTON_REGISTER = (By.CSS_SELECTOR, ".btn-bottom-register")
 button_register = browser.find_element(*BUTTON_REGISTER)
 browser.execute_script("return arguments[0].scrollIntoView(true);", button_register)
 button_register.click()
 
-
+# Send userdata to registration form
 USER_NAME_FIELD = (By.CSS_SELECTOR, ".userName")
 USER_EMAIL_FIELD = (By.CSS_SELECTOR, ".userEmail")
 USER_PHONE_FIELD = (By.CSS_SELECTOR, ".userPhone")
@@ -33,7 +35,8 @@ BUTTON_REGISTER_FORM = (By.CSS_SELECTOR, ".inputform .btn-register")
 browser.find_element(*USER_NAME_FIELD).send_keys(user_name)
 browser.find_element(*USER_EMAIL_FIELD).send_keys(user_email)
 browser.find_element(*USER_PHONE_FIELD).send_keys(user_phone_number)
-# browser.find_element(*BUTTON_REGISTER_FORM).click()
+browser.find_element(*BUTTON_REGISTER_FORM).click()
 
 time.sleep(5)
+print(f"Congratulation! You registered to webinars {current_month}-{current_year} from rebrainme.com")
 browser.close()
